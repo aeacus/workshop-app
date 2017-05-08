fa4f8a90f16d71ea59f269792f0dad1f77347eab
Mark Anderson <mark@chef.io>
2017-05-08 23:28:46 +0000

    Add other components

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoViewerComponent } from './video-viewer/video-viewer.component';
+import { StatFiltersComponent } from './stat-filters/stat-filters.component';
 
 const dashboardRoutes: Route[] = [
     { path: '', component: DashboardComponent, pathMatch: 'full' },
@@ -12,6 +15,6 @@ const dashboardRoutes: Route[] = [
     CommonModule,
     RouterModule.forChild(dashboardRoutes)
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, VideoListComponent, VideoViewerComponent, StatFiltersComponent]
 })
 export class DashboardModule { }
