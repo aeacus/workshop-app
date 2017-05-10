63afa350acbf65577e6bd1e52dc92c7208e40f35
Mark Anderson <mark@chef.io>
2017-05-10 16:10:55 +0000

    WIP; loading works from local asset store

--- src/app/video-list-loader.service.ts
+++ src/app/video-list-loader.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+const API_URL = "http://localhost:8085";
 
 @Injectable()
 export class VideoListLoaderService {
+  constructor(private http: Http) { }
 
-  constructor() { }
+  load() {
+    return this.http.get(API_URL + '/video');
+  }
 
 }
--- src/app/video-list.service.ts
+++ src/app/video-list.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+
+//import { VideoListLoaderService } from './video-list-loader-service';
 
 import { IVideo, fakeVideoData } from './api-types';
+
+const API_URL = "/assets";
 
 @Injectable()
 export class VideoListService {
   videoList: IVideo[]; 
 
+  videosNew: Observable<{}[]>; // type this as an IVideo?
+
   videos() {
     return this.videoList;
   }
 
-  constructor() { 
-       this.videoList = fakeVideoData;
+  constructor(private http: Http) { 
+    this.videoList = fakeVideoData;
+      
+    const videoObs = this.http.get(API_URL + '/videos.json');
+
+    this.videosNew = videoObs
+      .do(res => console.log(res))
+      .map(res => res.json());
   }
 
 }
